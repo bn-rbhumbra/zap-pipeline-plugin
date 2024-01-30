ff90eea98740ec9a1408578eaa87a9198129b01b
rbhumbra <rbhumbra@barracuda.com>
2024-01-30 11:30:11 +0000

    fix incorrect use of onSuccess for file import

--- src/main/java/com/vrondakis/zap/workflow/ImportZapUrlsExecution.java
+++ src/main/java/com/vrondakis/zap/workflow/ImportZapUrlsExecution.java
@@ -3,46 +3,45 @@ package com.vrondakis.zap.workflow;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+import com.vrondakis.zap.ZapDriver;
+import com.vrondakis.zap.ZapDriverController;
 import com.vrondakis.zap.ZapExecutionException;
 import org.jenkinsci.plugins.workflow.steps.StepContext;
 
-import com.vrondakis.zap.ZapDriver;
-import com.vrondakis.zap.ZapDriverController;
-
 public class ImportZapUrlsExecution extends DefaultStepExecutionImpl {
-    private ImportZapUrlsStepParameters importZapUrlsStepParameters;
+	private ImportZapUrlsStepParameters importZapUrlsStepParameters;
 
-    public ImportZapUrlsExecution(StepContext context, ImportZapUrlsStepParameters importZapUrlsStepParameters) {
-        super(context);
-        this.importZapUrlsStepParameters = importZapUrlsStepParameters;
-    }
+	public ImportZapUrlsExecution(StepContext context, ImportZapUrlsStepParameters importZapUrlsStepParameters) {
+		super(context);
+		this.importZapUrlsStepParameters = importZapUrlsStepParameters;
+	}
 
-    @Override
-    public boolean start() {
-        listener.getLogger().println("zap: Importing list of URLs...");
+	@Override
+	public boolean start() {
+		listener.getLogger().println("zap: Importing list of URLs...");
 
-        if (importZapUrlsStepParameters == null || importZapUrlsStepParameters.getPath().isEmpty()) {
-            getContext().onFailure(new ZapExecutionException("Could not load URLs file.", listener.getLogger()));
-            return false;
-        }
+		if (importZapUrlsStepParameters == null || importZapUrlsStepParameters.getPath().isEmpty()) {
+			getContext().onFailure(new ZapExecutionException("Could not load URLs file.", listener.getLogger()));
+			return false;
+		}
 
-        ZapDriver zapDriver = ZapDriverController.getZapDriver(this.run, listener.getLogger());
+		ZapDriver zapDriver = ZapDriverController.getZapDriver(this.run, listener.getLogger());
 
-        try {
-            zapDriver.importUrls(importZapUrlsStepParameters.getPath());
-        } catch (Exception e) {
-            getContext().onSuccess(new ZapExecutionException("Failed to load list of URLs at " + importZapUrlsStepParameters.getPath(), e, listener.getLogger()));
-            return false;
-        }
+		try {
+			zapDriver.importUrls(importZapUrlsStepParameters.getPath());
+		} catch (Exception e) {
+			getContext().onFailure(new ZapExecutionException("Failed to load list of URLs at " + importZapUrlsStepParameters.getPath(), e, listener.getLogger()));
+			return false;
+		}
 
-        getContext().onSuccess(true);
-        return true;
-    }
+		getContext().onSuccess(true);
+		return true;
+	}
 
-    // findbugs fails without this because "non-transient non-serializable instance field in serializable class"
-    private void writeObject(ObjectOutputStream out) {
-    }
+	// findbugs fails without this because "non-transient non-serializable instance field in serializable class"
+	private void writeObject(ObjectOutputStream out) {
+	}
 
-    private void readObject(ObjectInputStream in) {
-    }
+	private void readObject(ObjectInputStream in) {
+	}
 }
